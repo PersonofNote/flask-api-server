03ff9f1280351bed097a9fc9941dfc75b28f134a
Jessica Martin <apersonofnote@gmail.com>
2022-07-20 02:29:28 -0400

    ssl configuration

--- api/config.py
+++ api/config.py
@@ -13,6 +13,6 @@ class BaseConfig():
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'apidata.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "flask-app-secret-key-change-it"
-    JWT_SECRET_KEY = "jwt-app-secret-key-change-it"
+    SECRET_KEY = os.environ['SECRET_KEY']
+    JWT_SECRET_KEY = os.environ['JWS_SECRET_KEY']
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
